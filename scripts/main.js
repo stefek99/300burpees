9469b14fcd67293927c1a1065284890a754d857e
Michal Stefanow <mstefanow@gmail.com>
2015-07-06 23:16:10 +0100

    sliders

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,24 @@ app.filter('unsafe', ['$sce', function($sce){
 }]);
 
 
-app.controller("StartCtrl", function($scope) {
+
+app.filter('pad', [function(){
+  return function(text, width) { 
+    text = text + '';
+    return text.length >= width ? text : new Array(width - text.length + 1).join('0') + text;
+  };
+}]);
+
+
+app.controller("StartCtrl", function($rootScope, $scope, $window) {
+
+  $rootScope.howmany = 20;
+  $rootScope.time = {
+    hours: 0,
+    minutes: 3,
+    seconds: 0
+  };
+
   $scope.message = "start";
 });
 
