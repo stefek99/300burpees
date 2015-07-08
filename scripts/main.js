628d3cd89b843283f057281877997139a13116f9
Michal Stefanow <mstefanow@gmail.com>
2015-07-08 23:00:20 +0100

    Some basic couner functionality

--- scripts/main.js
+++ scripts/main.js
@@ -25,17 +25,39 @@ app.filter('unsafe', ['$sce', function($sce){
   };
 }]);
 
-
-
 app.filter('pad', [function(){
   return function(text, width) { 
-    text = text + '';
+    text = text + ''; // converting to string because if we pass number it will fail
     return text.length >= width ? text : new Array(width - text.length + 1).join('0') + text;
   };
 }]);
 
+app.filter('time', [function(){
+  return function(text) { // http://jsbin.com/gist/4372563?html,js,output
+    var h, m, s, ms; // https://jslinterrors.com/variable-a-was-not-declared-correctly
+    h = m = s = ms = 0;
 
-app.controller("StartCtrl", function($rootScope, $scope, $window) {
+    var time = parseInt(text, 10);
+    var newTime = '';
+
+    h = Math.floor( time / (60 * 60 * 1000) );
+    time = time % (60 * 60 * 1000);
+    m = Math.floor( time / (60 * 1000) );
+    time = time % (60 * 1000);
+    s = Math.floor( time / 1000 );
+    ms = time % 1000;
+
+    var pad = function(num, size) { // TODO: use only one pad function
+      var s = "0000" + num;
+      return s.substr(s.length - size);
+    };
+
+    newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+    return newTime;
+  };
+}]);
+
+app.controller("StartCtrl", function($rootScope, $scope, $location, $window) {
 
   $rootScope.howmany = 20;
   $rootScope.time = {
@@ -44,9 +66,37 @@ app.controller("StartCtrl", function($rootScope, $scope, $window) {
     seconds: 0
   };
 
-  $scope.message = "start";
+  $scope.start = function() {
+    $location.path("/countdown");
+  };
 });
 
-app.controller("CountdownCtrl", function($scope) {
-  $scope.message = "countdown";
+app.controller("CountdownCtrl", function($scope, CountdownService) {
+  CountdownService.start();
+
+  $scope.elapsed = CountdownService.elapsed;
+
+  $scope.ten = function() {
+
+  };
+});
+
+app.service("CountdownService", function($interval) {
+  var startTimestamp;
+  var elapsed = {
+    time : 0
+  };
+
+  var start = function() {
+    startTimestamp = new Date().getTime();
+    $interval(function() {
+      var now = new Date().getTime();
+      elapsed.time = now - startTimestamp;
+    }, 51); // some funny number so it looks nice
+  };
+
+  return {
+    start: start,
+    elapsed : elapsed
+  };
 });
