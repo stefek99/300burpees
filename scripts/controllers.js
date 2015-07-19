5e34f3a4b248e638de35b7d411f0297038ab86ce
Michal Stefanow <mstefanow@gmail.com>
2015-07-19 19:34:25 +0100

    redo

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -17,37 +17,42 @@ app.controller("CountdownCtrl", function($rootScope, $scope, $location, Countdow
   var secondsForOne = 1000 * totalSeconds / $rootScope.time.howmany;
   console.log("secondsForOne: " + secondsForOne);
   
-
   CountdownService.start();
-  var howmany = 0;
-  times = [{howmany: 0, time: 0}];
 
+  $scope.times = [];
   $scope.elapsed = CountdownService.elapsed; // need to pass object (because primitive types are copied)
   
-  $scope.ten = function() {
-    howmany += 10;
-    
-    if (howmany >= $rootScope.time.howmany) { // YEAH! Finish! Hurray!
-      if ($scope.finalTime) {
-        return;
-      }
-      CountdownService.stop(); // We also need to display some helpful messages and offer new options etc...
-      $scope.finalTime = CountdownService.elapsed.time; // In this place we want primitive object to avoid copying
-      times.push({howmany : howmany, time: CountdownService.elapsed.time});
-    } else { // Not finish yet, more to go...
-      times.push({howmany : howmany, time: CountdownService.elapsed.time});
-    }
-  };
 
-  $scope.$watch(function() {
-    return times;
-  }, function() {
-    console.log(times);
-    $scope.times = []; // RISKY! (we are changing watched value in a watch function)
-    for (var i=1; i<times.length; i++) { // zaczynamy od pierwszego
-      var currentTenTime = times[i].time - times[i-1].time;
-      var plusTooSlow = (currentTenTime - (secondsForOne*10)) > 0;
-      $scope.times.push({current : currentTenTime, plus : plusTooSlow, difference : currentTenTime - secondsForOne});
-    }
-  }, true);
+  for (var i=10; i<=$rootScope.time.howmany; i+=10) {
+    $scope.times.push({ "howmany" : i});
+  }
+
+
+
+  // $scope.ten = function() {
+  //   howmany += 10;
+    
+  //   if (howmany >= $rootScope.time.howmany) { // YEAH! Finish! Hurray!
+  //     if ($scope.finalTime) {
+  //       return;
+  //     }
+  //     CountdownService.stop(); // We also need to display some helpful messages and offer new options etc...
+  //     $scope.finalTime = CountdownService.elapsed.time; // In this place we want primitive object to avoid copying
+  //     times.push({howmany : howmany, time: CountdownService.elapsed.time});
+  //   } else { // Not finish yet, more to go...
+  //     times.push({howmany : howmany, time: CountdownService.elapsed.time});
+  //   }
+  // };
+
+  // $scope.$watch(function() {
+  //   return times;
+  // }, function() {
+  //   console.log(times);
+  //   $scope.times = []; // RISKY! (we are changing watched value in a watch function)
+  //   for (var i=1; i<times.length; i++) { // zaczynamy od pierwszego
+  //     var currentTenTime = times[i].time - times[i-1].time;
+  //     var plusTooSlow = (currentTenTime - (secondsForOne*10)) > 0;
+  //     $scope.times.push({current : currentTenTime, plus : plusTooSlow, difference : currentTenTime - secondsForOne});
+  //   }
+  // }, true);
 });
